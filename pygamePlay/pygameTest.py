ca8b687b8f58cbc2f300a7079b3fd9de487764fb
RHSMcLain <amclain@riverdale.k12.or.us>
2025-09-16 10:10:42 -0700

    Updated comments, and alternate screen colors

--- pygamePlay/pygameTest.py
+++ pygamePlay/pygameTest.py
@@ -4,10 +4,12 @@ pygame.init()
 screen = pygame.display.set_mode((1280,720))
 clock = pygame.time.Clock()
 running = True
-eTime = 0
+eTime = 0 #using this to explore changing the background after a second
 color1 = "purple"
 color2 = pygame.Color(23, 190, 209)
 currColor = color1
+
+#playerPos will hold the location of the player circle. Starts in the middle
 playerPos = pygame.Vector2(screen.get_width()/2, screen.get_height()/2)
 dt = 0 #delta time
 
@@ -16,22 +18,25 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    eTime += clock.get_time()
+    eTime += clock.get_time() #just adding up how much time has passed
     if (eTime >= 1000):
         currColor = color2
+        if (eTime >= 2000):
+            eTime = 0
+            currColor = color1
     screen.fill(currColor)
     pygame.draw.circle(screen,"white", playerPos, 60)
 
     #listen for input
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
-        playerPos.y -= 300 * dt
+        playerPos.y -= 300 * dt #moves the player position up
     if keys[pygame.K_a]:
-        playerPos.x -= 300 * dt
+        playerPos.x -= 300 * dt # left
     if keys[pygame.K_s]:
-        playerPos.y += 300 * dt
+        playerPos.y += 300 * dt # down
     if keys[pygame.K_d]:
-        playerPos.x += 300 * dt
+        playerPos.x += 300 * dt # right
     #render the screen
     pygame.display.flip()
     dt = clock.tick(60) / 1000
