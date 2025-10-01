c378b1a50d0d5847ae0fc8c563f739d7325d9294
RHSMcLain <amclain@riverdale.k12.or.us>
2025-10-01 09:51:46 -0700

    editlists

--- LoopsAndLists/editLists.py
+++ LoopsAndLists/editLists.py
@@ -7,17 +7,34 @@ def addBook():
     books.append(newTitle)
 
 def listBooks():
-    for book in books:
-        print(book)
+    # for book in books:
+    #     print(book)
+    # to get the length of the list (number of items, use the len() function)
+    print(len(books))
+    for i in range(len(books)):
+        print(str(i + 1) + ": " + books[i])
 
+
+def editBook():
+    #what do we need?
+    #TODO: refactor for a select books function to be used by edit and remove book functions
+    listBooks()
+    print("-----")
+    bookToEdit = input("Which one would you like to change?")
+    #TODO: Validate input as an int in the range of the list
+    bookToEdit = int(bookToEdit) -1
+    print(" WE will edit book " + books[bookToEdit])
+    
 while True:
     #menu part
-    action = input("What would you like to do? (A)dd book, (L)ist books , (Q)uit>> ")
+    action = input("What would you like to do? (A)dd book, (L)ist books, (E)dit book , (Q)uit>> ")
     action = action[:1].lower()
     if (action == "a"):
         addBook()
     elif action == "l":
         listBooks()
+    elif action == "e":
+        editBook()    
     elif action == "q":
         break
     else:
