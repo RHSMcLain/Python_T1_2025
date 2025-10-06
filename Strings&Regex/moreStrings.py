11e6a036b75e4d72964543c26c51d25f6760733d
RHSMcLain <amclain@riverdale.k12.or.us>
2025-10-06 09:45:03 -0700

    built framework for morestrings demo

---
+++ Strings&Regex/moreStrings.py
@@ -0,0 +1,24 @@
+
+#formatting
+
+#Quotes inside quotes
+
+#multiline Strings
+
+#Strings as arrays
+
+
+#Check contents
+
+#slicing strings (and lists for that matter)
+
+
+#modifying
+
+
+
+
+#other methods
+
+
+
