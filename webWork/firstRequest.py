935f6670ae99c977a2dea307ec96cbde4b219eb0
RHSMcLain <amclain@riverdale.k12.or.us>
2025-11-13 09:04:44 -0800

    Added request demo

--- webWork/firstRequest.py
+++ webWork/firstRequest.py
@@ -1,8 +1,24 @@
-import requests
+import requests, shelve
 from bs4 import BeautifulSoup
+
 x = requests.get('https://riverdalehs.com')
 soup = BeautifulSoup(x.text)
 #print(soup.prettify())
-newsTitles = soup.find_all(class_="fsTitle")
+newsSection = soup.find(id="fsEl_3863")
+newsTitles = newsSection.find_all(class_="fsTitle")
+newNews = []
 for title in newsTitles:
-    print(title.get_text())
+    print(title.get_text().strip())
+    newNews.append(title.get_text().strip())
+with shelve.open("newsFile") as f:
+    if 'news' not in f:
+        f['news'] = []
+    oldNews = f['news']
+    if (oldNews == newNews):
+        print("No new news")
+    else:
+        print("That's new")
+        f['news'] = newNews
+
+
+
